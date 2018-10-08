c41744843b32c81b35bf7e0f397366d53f4c60af
Richie Madrigal <richie.madrigal@icloud.com>
2018-10-08 19:37:54 -0400

    updated game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,104 +1,154 @@
-$(document).ready(function () {
-	var targetNumber = Math.floor(Math.random() * 101) + 19;
+// A variable that ouputs a random number for the users target number
+let targetNumber = Math.floor(Math.random() * 101) + 19;
 
+// We target the "number-to-guess" element from the html and we add a .text 
+// method that will display the actual number as text on the html
+$("#number-to-guess").text(targetNumber);
+
+// Generate random numbers for the 4 crystals 
+let blueCrystal = Math.floor(Math.random() * 11) + 1;
+let redCrystal = Math.floor(Math.random() * 11) + 1;
+let goldCrystal = Math.floor(Math.random() * 11) + 1;
+let whiteCrystal = Math.floor(Math.random() * 11) + 1;
+
+let wins = 0;
+let losses = 0;
+let userScore = 0;
+
+// Create a function to target the specific variables I want to reset each time the user wins or loses
+function reset() {
+	// Reset the target number with a random number
+	targetNumber = Math.floor(Math.random() * 101) + 19;
+	// target the number-to-guess id to display the new random number eveytime this function is called 
 	$("#number-to-guess").text(targetNumber);
 
-	var blueCrystal = Math.floor(Math.random() * 11) + 1;
-	var redCrystal = Math.floor(Math.random() * 11) + 1;
-	var goldCrystal = Math.floor(Math.random() * 11) + 1;
-	var whiteCrystal = Math.floor(Math.random() * 11) + 1;
+	// Reset each crystals values with a random number
+	blueCrystal = Math.floor(Math.random() * 11) + 1;
+	redCrystal = Math.floor(Math.random() * 11) + 1;
+	goldCrystal = Math.floor(Math.random() * 11) + 1;
+	whiteCrystal = Math.floor(Math.random() * 11) + 1;
+	userScore = 0;
+	// We target the score id from the html so eveytime this reset function is called it clears/removes the users previous score from the display
+	$('#score').text(userScore);
 
-	var counter = 0;
-	var wins = 0;
-	var losses = 0;
+};
 
-// Here I create a function where I target specific variables I want to reset each time the user wins or loses
-	function reset(){
-		targetNumber = Math.floor(Math.random() * 101) + 19;
-		$("#number-to-guess").text(targetNumber);
-		blueCrystal = Math.floor(Math.random() * 11) + 1;
-		redCrystal = Math.floor(Math.random() * 11) + 1;
-		goldCrystal = Math.floor(Math.random() * 11) + 1;
-		whiteCrystal = Math.floor(Math.random() * 11) + 1;
-		counter = 0;
-		$("#score").text(counter);
+// On click function that targets the first blue crystal. When the blue crystal is clicked the userScore will 
+// take its current value and add the blueCrystal random # it generates from the above code ln. 10 
+
+$("#crystalOne").on('click', function () {
+
+	// userScore = its current value plus the randomly generated number blueCrystal holds
+	userScore += blueCrystal;
+
+	// we target the score id in the html and apply the text method to display the number on sceen
+	$('#score').text(userScore);
+
+	// Conditional that checks if userScore is equal the games target number and if so...
+	// Alert the user they won, than add a win to the wins variable; and finally target the wins id in the html so we can display to the user 
+	if (userScore == targetNumber) {
+		alert('Congratulations you won this round');
+		wins++;
+		$('#wins').text(wins);
+		// Runs the reset function above
+		reset();
+	// If conditional is falsy (the users score went over the target number), than alert user of their lost; add a loss to the losses variable 
+	// target the losses id in the html and display the losses counter to user 
+	} else if (userScore > targetNumber) {
+		alert('You lost this round.');
+		losses++;
+		$('#losses').text(losses);
+		reset();
+
 	}
-
-
-	$("#crystalOne").on('click', function() {
-		counter += blueCrystal;
-		$("#score").text(counter); 
-		if (counter == targetNumber){
-			alert('Congratulations, you won this round!');
-			wins++;
-			document.querySelector("#wins").innerHTML = wins;
-
-			reset();
-		}
-		else if (counter > targetNumber){
-			alert('You lost this round. Better luck next game!');
-			losses++;
-			document.querySelector("#losses").innerHTML = losses;
-			
-			reset();
-		}
-	})
-
-	$("#crystalTwo").on('click', function() {
-		counter += redCrystal;
-		$("#score").text(counter); 
-		if (counter == targetNumber){ 
-			alert('Congratulations, you won this round!');
-			wins++;
-			document.querySelector("#wins").innerHTML = wins;
-
-			reset();
-		}
-		else if (counter > targetNumber){
-			alert('You lost this round. Better luck next game!');
-			losses++;
-			document.querySelector("#losses").innerHTML = losses;
-
-			reset();
-		} 
-	})
-
-	$("#crystalThree").on('click', function() {
-		counter += goldCrystal;
-		$("#score").text(counter);
-		if (counter == targetNumber){
-			alert('Congratulations, you won this round!');
-			wins++;
-			document.querySelector("#wins").innerHTML = wins;
-
-			reset();
-		}
-		else if (counter > targetNumber){
-			alert('You lost this round. Better luck next game!');
-			losses++;
-			document.querySelector("#losses").innerHTML = losses;
-
-			reset();
-		}
-	})
-
-	$("#crystalFour").on('click', function() {
-		counter += whiteCrystal;
-		$("#score").text(counter);
-		if (counter == targetNumber){
-			alert('Congratulations, you won this round!');
-			wins++;
-			document.querySelector("#wins").innerHTML = wins;
-		
-			reset();
-		}
-		else if (counter > targetNumber){
-			alert('You lost this round. Better luck next game!');
-			losses++;
-			document.querySelector("#losses").innerHTML = losses;
-
-			reset();
-		}
-	})
-
 });
+
+
+// On click function that targets the second red crystal. When the red crystal is clicked the userScore will 
+// take its current value and add the redCrystal random # it generates from the above code ln. 11 
+
+$("#crystalTwo").on('click', function () {
+
+	// userScore = its current value plus the randomly generated number redCrystal holds
+	userScore += redCrystal;
+
+	// we target the score id in the html and apply the text method to display the number on sceen
+	$('#score').text(userScore);
+
+	// Conditional that checks if userScore is equal the games target number and if so...
+	// Alert the user they won, than add a win to the wins variable; and finally target the wins id in the html so we can display to the user 
+	if (userScore == targetNumber) {
+		alert('Congratulations you won this round');
+		wins++;
+		$('#wins').text(wins);
+		// Runs the reset function above
+		reset();
+		// If conditional is falsy (the users score went over the target number), than alert user of their lost; add a loss to the losses variable 
+		// target the losses id in the html and display the losses counter to user 
+	} else if (userScore > targetNumber) {
+		alert('You lost this round.');
+		losses++;
+		$('#losses').text(losses);
+		reset();
+
+	}
+});
+// On click function that targets the third gold crystal. When the gold crystal is clicked the userScore will 
+// take its current value and add the goldCrystal random # it generates from the above code ln. 13 
+
+$("#crystalThree").on('click', function () {
+
+	// userScore = its current value plus the randomly generated number goldCrystal holds
+	userScore += goldCrystal;
+
+	// we target the score id in the html and apply the text method to display the number on sceen
+	$('#score').text(userScore);
+
+	// Conditional that checks if userScore is equal the games target number and if so...
+	// Alert the user they won, than add a win to the wins variable; and finally target the wins id in the html so we can display to the user 
+	if (userScore == targetNumber) {
+		alert('Congratulations you won this round');
+		wins++;
+		$('#wins').text(wins);
+		// Runs the reset function above
+		reset();
+		// If conditional is falsy (the users score went over the target number), than alert user of their lost; add a loss to the losses variable 
+		// target the losses id in the html and display the losses counter to user 
+	} else if (userScore > targetNumber) {
+		alert('You lost this round.');
+		losses++;
+		$('#losses').text(losses);
+		reset();
+
+	}
+});
+// On click function that targets the fourth white crystal. When the white crystal is clicked the userScore will 
+// take its current value and add the whiteCrystal random # it generates from the above code ln. 14 
+
+$("#crystalFour").on('click', function () {
+
+	// userScore = its current value plus the randomly generated number whiteCrystal holds
+	userScore += whiteCrystal;
+
+	// we target the score id in the html and apply the text method to display the number on sceen
+	$('#score').text(userScore);
+
+	// Conditional that checks if userScore is equal the games target number and if so...
+	// Alert the user they won, than add a win to the wins variable; and finally target the wins id in the html so we can display to the user 
+	if (userScore == targetNumber) {
+		alert('Congratulations you won this round');
+		wins++;
+		$('#wins').text(wins);
+		// Runs the reset function above
+		reset();
+		// If conditional is falsy (the users score went over the target number), than alert user of their lost; add a loss to the losses variable 
+		// target the losses id in the html and display the losses counter to user 
+	} else if (userScore > targetNumber) {
+		alert('You lost this round.');
+		losses++;
+		$('#losses').text(losses);
+		reset();
+
+	}
+});
+
